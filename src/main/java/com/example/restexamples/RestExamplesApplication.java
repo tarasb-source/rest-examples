d56dfaf4e0494e757adc3afed1b7d38a1d0c756c
tarasb-source” <taras.brytskiy@gmail.com>
2026-06-02 19:30:11 -0400

    Init Commit

---
+++ src/main/java/com/example/restexamples/RestExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.restexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restexamples/RestExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
